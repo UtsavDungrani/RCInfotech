cebe4c23109bafe2ae6b503cc65ab7c2fd93096e
RCInfotech <rcinfotech11@gmail.com>
2025-06-15 08:10:39 +0530

    Initial commit

--- index.php
+++ index.php
@@ -902,27 +902,129 @@ $services = $stmt1->fetchAll(PDO::FETCH_ASSOC);  // Changed to use $stmt1 and st
   <?php include 'footer.php'; ?>
   <!-- end footer -->
   <!-- js section -->
-  <script src="js/jquery.min.js" defer></script>
-  <script src="js/bootstrap.min.js" defer></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
   <!-- menu js -->
-  <script src="js/menumaker.js" defer></script>
+  <script src="js/menumaker.js"></script>
   <!-- wow animation -->
-  <script src="js/wow.js" defer></script>
+  <script src="js/wow.js"></script>
   <!-- custom js -->
-  <script src="js/custom.js" defer></script>
+  <script src="js/custom.js"></script>
   <!-- revolution js files -->
-  <script src="revolution/js/jquery.themepunch.tools.min.js" defer></script>
-  <script src="revolution/js/jquery.themepunch.revolution.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.actions.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.carousel.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.kenburn.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.layeranimation.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.migration.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.navigation.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.parallax.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.slideanims.min.js" defer></script>
-  <script src="revolution/js/extensions/revolution.extension.video.min.js" defer></script>
-  <script src="js/security.js" defer></script>
+  <script src="revolution/js/jquery.themepunch.tools.min.js"></script>
+  <script src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+  <script src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+  <script src="js/security.js"></script>
+  <script>
+    jQuery(document).ready(function () {
+      if (jQuery("#rev_slider_4_1").revolution == undefined) {
+        revslider_showDoubleJqueryError("#rev_slider_4_1");
+      } else {
+        jQuery("#rev_slider_4_1").show().revolution({
+          sliderType: "standard",
+          jsFileLocation: "revolution/js/",
+          sliderLayout: "fullwidth",
+          dottedOverlay: "none",
+          delay: 9000,
+          navigation: {
+            keyboardNavigation: "off",
+            keyboard_direction: "horizontal",
+            mouseScrollNavigation: "off",
+            mouseScrollReverse: "default",
+            onHoverStop: "on",
+            touch: {
+              touchenabled: "on",
+              swipe_threshold: 75,
+              swipe_min_touches: 1,
+              swipe_direction: "horizontal",
+              drag_block_vertical: false
+            },
+            arrows: {
+              style: "zeus",
+              enable: true,
+              hide_onmobile: true,
+              hide_under: 600,
+              hide_onleave: true,
+              hide_delay: 200,
+              hide_delay_mobile: 1200,
+              tmp: '<div class="tp-title-wrap">    <div class="tp-arr-imgholder"></div> </div>',
+              left: {
+                h_align: "left",
+                v_align: "center",
+                h_offset: 20,
+                v_offset: 0
+              },
+              right: {
+                h_align: "right",
+                v_align: "center",
+                h_offset: 20,
+                v_offset: 0
+              }
+            },
+            bullets: {
+              enable: true,
+              hide_onmobile: true,
+              hide_under: 600,
+              style: "zeus",
+              hide_onleave: true,
+              hide_delay: 200,
+              hide_delay_mobile: 1200,
+              direction: "horizontal",
+              h_align: "center",
+              v_align: "bottom",
+              h_offset: 0,
+              v_offset: 50,
+              space: 5,
+              tmp: '<span class="tp-bullet-img-wrap"><span class="tp-bullet-image"></span></span><span class="tp-bullet-title">{{title}}</span>'
+            }
+          },
+          viewPort: {
+            enable: true,
+            outof: "pause",
+            visible_area: "80%",
+            presize: false
+          },
+          responsiveLevels: [1240, 1024, 778, 480],
+          visibilityLevels: [1240, 1024, 778, 480],
+          gridwidth: [1240, 1024, 778, 480],
+          gridheight: [600, 500, 400, 300],
+          lazyType: "none",
+          parallax: {
+            type: "scroll",
+            origo: "slidercenter",
+            speed: 400,
+            levels: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 46, 47, 48, 49, 50, 55],
+            type: "scroll",
+          },
+          shadow: 0,
+          spinner: "off",
+          stopLoop: "off",
+          stopAfterLoops: -1,
+          stopAtSlide: -1,
+          shuffle: "off",
+          autoHeight: "off",
+          hideThumbsOnMobile: "off",
+          hideSliderAtLimit: 0,
+          hideCaptionAtLimit: 0,
+          hideAllCaptionAtLilmit: 0,
+          debugMode: false,
+          fallbacks: {
+            simplifyAll: "off",
+            nextSlideOnWindowFocus: "off",
+            disableFocusListener: false,
+          }
+        });
+      }
+    });
+  </script>
 </body>
 
 </html>
